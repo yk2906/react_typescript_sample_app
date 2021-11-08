57b401f26124edcff4d73547bd1901289577bd98
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-08 15:07:10 +0900

    test

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,40 @@
+import { Box, Flex, Heading, IconButton, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
-  return <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+      padding={{ base: 3, md: 5 }}
+    >
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          ユーザー管理アプリ
+        </Heading>
+      </Flex>
+      <Flex
+        align="center"
+        fontSize="sm"
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+      >
+        <Box pr={4}>
+          <Link>ユーザー一覧</Link>
+          <Link>設定</Link>
+        </Box>
+      </Flex>
+      <IconButton
+        aria-label="メニューボタン"
+        icon={<HamburgerIcon />}
+        size="sm"
+        variant="unstyled"
+        display={{ base: "block", md: "none" }}
+      />
+    </Flex>
+  );
 });
