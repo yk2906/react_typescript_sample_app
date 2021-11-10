cba84b2a2eb3143d430581a36502c7450579d5d1
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-10 17:27:11 +0900

    test

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -51,7 +51,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
