8d0af15fab4ef1411e542413ce68eb8cb1c29ddb
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-10 16:17:08 +0900

    test

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,32 +5,35 @@ import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
 import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
 
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route
-        path="/home"
-        render={({ match: { url } }) => (
-          <Switch>
-            {homeRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
-                <HeaderLayout>{route.children}</HeaderLayout>
-              </Route>
-            ))}
-          </Switch>
-        )}
-      />
-      <Route path="*">
-        <Page404 />
-      </Route>
+      <LoginUserProvider>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route
+          path="/home"
+          render={({ match: { url } }) => (
+            <Switch>
+              {homeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayout>{route.children}</HeaderLayout>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
+        <Route path="*">
+          <Page404 />
+        </Route>
+      </LoginUserProvider>
     </Switch>
   );
 });
